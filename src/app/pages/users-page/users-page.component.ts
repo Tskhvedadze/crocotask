1003a3d31351c4a2e91cf6ca0290aa9dd9a23d2b
Tskhvedadze <giorgicxvedadze4@gmail.com>
2024-07-12 09:00:18 +0400

    completed users and posts page

---
+++ src/app/pages/users-page/users-page.component.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { AllUsersService } from '../../services/allusers.service';
+
+@Component({
+  selector: 'app-users-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './users-page.component.html',
+  styleUrl: './users-page.component.css',
+})
+export class UsersPageComponent implements OnInit {
+  users: any = [];
+
+  constructor(private allUsersService: AllUsersService) {}
+
+  ngOnInit(): void {
+    this.fetchUsers();
+  }
+
+  fetchUsers(): void {
+    this.allUsersService.getAllPosts().subscribe({
+      next: (response) => {
+        this.users = response;
+        console.log(this.users[0]);
+      },
+      error: (err) => console.log('Error fetching users', err),
+    });
+  }
+}
